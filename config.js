a0856e8cc2f18f4a6d3f5f4fb6baa4e17ed65875
Emadstrong <132583034+Emadstrong@users.noreply.github.com>
2024-02-18 02:32:52 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,56 @@
+{
+  "content": "||@everyone||",
+  "tts": false,
+  "embeds": [
+    {
+      "id": 956052606,
+      "description": ".\n-  ** مؤثرين يعني اليوتوبرز و الستريمرز #** -\n\n- ويمكنك الانضمام الان €\n\n- اذا كان عندك فائدة لليوتيوبرز او الستريمرز!\n\n- كمثل ان تكون ممنتج او مصمم مثلاً! \nاو ``حتى يوتيوبر او ستريمر !``\n\n- لاكن مو اي يوتيوبر او ستريمر ممكن ينضم معنا !\n``فهذا يعني اذا انضميت معنا معناها انت شخص مميز فعلاً!``",
+      "fields": [],
+      "title": "🌎💫 سيرفر مؤثرين فيتك!",
+      "color": 5006713,
+      "author": {
+        "name": "ViTK | INFLUENCERS",
+        "url": "https://discord.com/invite/9YP76AwhZV",
+        "icon_url": "https://cdn.discordapp.com/attachments/1203981520214302730/1207770505336332288/0215-06_1708018751566_1.gif?ex=65e0daef&is=65ce65ef&hm=126dbd2994299d8e40daaa672f8a6b720023018478e44acef537b225c7541d5e&"
+      },
+      "footer": {
+        "icon_url": "https://cdn.discordapp.com/attachments/1203981520214302730/1207770435836448788/0215-06_1708018751566_4.gif?ex=65e0dadf&is=65ce65df&hm=397104a644423932775b01d6c19d192e60e37c7542d98b21e5d552ff837196d1&",
+        "text": "ViTK X TMTM"
+      },
+      "image": {
+        "url": "https://cdn.discordapp.com/attachments/1203981520214302730/1207770485832687646/0215-06_1708018751566_3.gif?ex=65e0daeb&is=65ce65eb&hm=2a91df269c039e90e7d5e59bb8ce7d37c2629e5718dc5dbbbf2c12c9beab71cc&"
+      },
+      "thumbnail": {
+        "url": "https://cdn.discordapp.com/attachments/1203981520214302730/1207770485832687646/0215-06_1708018751566_3.gif?ex=65e0daeb&is=65ce65eb&hm=2a91df269c039e90e7d5e59bb8ce7d37c2629e5718dc5dbbbf2c12c9beab71cc&"
+      }
+    }
+  ],
+  "components": [
+    {
+      "id": 526004767,
+      "type": 1,
+      "components": [
+        {
+          "id": 58015139,
+          "type": 2,
+          "style": 5,
+          "label": "دخول",
+          "action_set_id": "475601959",
+          "emoji": {
+            "id": "1206774856818098227",
+            "name": "emoji_15",
+            "animated": true
+          },
+          "url": "https://discord.com/invite/9YP76AwhZV"
+        }
+      ]
+    }
+  ],
+  "actions": {
+    "475601959": {
+      "actions": []
+    }
+  },
+  "username": "ViTK",
+  "avatar_url": "https://cdn.discordapp.com/avatars/1207378004754436166/3548f5375033c8db8d6a6f49d6a4c4ff.png?size=1024"
+}
